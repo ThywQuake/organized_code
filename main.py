43ac99e82ea7f038285c0e9e6de42d77a42e638c
thyw <thywquake@foxmail.com>
2025-11-30 17:49:32 +0800

    small fixes

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+
+
 def main():
     print("Hello from organized-code!")
+    data_folder = "data/"
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+        os.makedirs(os.path.join(data_folder, "clean"))
+    print(f"Data folder '{data_folder}' is ready.")
+
+    output_folder = "output/"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        os.makedirs(os.path.join(output_folder, "model"))
+        os.makedirs(os.path.join(output_folder, "eval"))
+        os.makedirs(os.path.join(output_folder, "pred"))
+    print(f"Output folder '{output_folder}' is ready.")
 
 
 if __name__ == "__main__":
